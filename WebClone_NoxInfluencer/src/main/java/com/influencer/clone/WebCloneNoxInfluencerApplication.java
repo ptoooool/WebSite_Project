9ef8c50c62cc5db4fbb16f880936124721f2878e
ptoooool <55444828+ptoooool@users.noreply.github.com>
2020-06-23 17:33:43 +0900

    Create Influencer Website Project
    Start Project

---
+++ WebClone_NoxInfluencer/src/main/java/com/influencer/clone/WebCloneNoxInfluencerApplication.java
@@ -0,0 +1,13 @@
+package com.influencer.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebCloneNoxInfluencerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCloneNoxInfluencerApplication.class, args);
+	}
+
+}
